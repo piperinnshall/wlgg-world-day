96130f216a5728d8c98151bbe35b88f362dbe89d
Piper Inns Hall <110881957+piperinnshall@users.noreply.github.com>
2025-04-22 22:20:45 +1200

    feat: convery belt working (#7)
    Co-authored-by: Filkspline <LeoRiginelli@gmail.com>

--- src/scripts/belt.gd
+++ src/scripts/belt.gd
@@ -2,14 +2,17 @@ extends Sprite2D
 
 @export var move_speed: float = 3.5
 
-var pos_x: float = 610
-var pos_y: float = 0
+var pos_x: float = 900
+var pos_y: float = 35
 
 func _init():
 	set_process(false)
 
 func _process(delta: float) -> void:
 	global_position += Vector2(-move_speed,  0)
+	if global_position.x < 66:
+		queue_free() 
+	
 
 func setup():
 	global_position = Vector2(pos_x, pos_y)
--- src/scripts/conveyor.gd
+++ src/scripts/conveyor.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 @onready var belt = preload("res://src/nodes/belt.tscn")
 
-var spawn_delay: float  = 1.0
+var spawn_delay: float  = 0.5
 var spawn_timer: float  = 0.0
 
 func _process(delta: float) -> void:
@@ -13,5 +13,7 @@ func _process(delta: float) -> void:
 
 func create_moving_belt():
 	var b_inst = belt.instantiate()
+	b_inst.z_as_relative = false
+	b_inst.z_index = 20
 	get_tree().get_root().call_deferred("add_child", b_inst)
 	b_inst.setup()
