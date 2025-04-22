05944d0221313d271d1c4d9775f2d8c8df63ac75
Piper Inns Hall <110881957+piperinnshall@users.noreply.github.com>
2025-04-22 23:34:52 +1200

    feat: Conveyor belts propgate on start (#8)

--- src/scripts/conveyor.gd
+++ src/scripts/conveyor.gd
@@ -1,19 +1,38 @@
 extends Node2D
 
-@onready var belt = preload("res://src/nodes/belt.tscn")
+@export var belt_scene: PackedScene = preload("res://src/nodes/belt.tscn")
+@export var spacing: float = 150.0
 
-var spawn_delay: float  = 0.5
-var spawn_timer: float  = 0.0
+var last_belt = null
 
-func _process(delta: float) -> void:
-	spawn_timer += delta
-	if spawn_timer >= spawn_delay:
-		create_moving_belt()
-		spawn_timer = 0.0
+func _ready() -> void:
+	propogate()
 
-func create_moving_belt():
-	var b_inst = belt.instantiate()
-	b_inst.z_as_relative = false
-	b_inst.z_index = 20
-	get_tree().get_root().call_deferred("add_child", b_inst)
-	b_inst.setup()
+func _process(_delta: float) -> void:
+	if not last_belt or last_belt.global_position.x <= last_belt.spawn_position.x - spacing:
+		spawn_belt()
+
+func spawn_belt() -> void:
+	var b = belt_scene.instantiate()
+	b.z_as_relative = false
+	b.z_index = 20
+	get_tree().get_root().call_deferred("add_child", b)
+	b.setup()
+	last_belt = b
+	
+func propogate():
+	var temp_belt = belt_scene.instantiate()
+	var start_x = temp_belt.spawn_position.x
+	var end_x = temp_belt.final_position
+	temp_belt.queue_free()
+
+	var pos_x = start_x
+	while pos_x >= end_x:
+		var b = belt_scene.instantiate()
+		b.z_as_relative = false
+		b.z_index = 20
+		get_tree().get_root().call_deferred("add_child", b)
+		b.setup()
+		b.global_position.x = pos_x
+		last_belt = b
+		pos_x -= spacing
