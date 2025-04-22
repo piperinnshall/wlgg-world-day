7415ebb53879bd59e40547ac5bc224b4232ed6ba
Piper Inns Hall <110881957+piperinnshall@users.noreply.github.com>
2025-04-22 17:13:29 +1200

    refactor: Update background script (#2)
    * feat: Begin adding queue
    
    * refactor: Update background script

--- src/scripts/key_listener.gd
+++ src/scripts/key_listener.gd
@@ -1,14 +1,21 @@
 extends Sprite2D
 
 @onready var key_moving = preload("res://src/nodes/key_moving.tscn")
-
 @export var key_name: String = ""
+
+var key_moving_queue = []
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed(key_name):
 		create_moving_key()
+	
+	if key_moving_queue.front().passed and key_moving_queue.size() > 0:
+		key_moving_queue.pop_front()
+		print("popped")
 
 func create_moving_key():
 	var km_inst = key_moving.instantiate()
 	get_tree().get_root().call_deferred("add_child", km_inst)
 	km_inst.setup(frame + 4)
+	
+	key_moving_queue.push_back(km_inst)
