e301ba1d5bc42bcf50a0ea5b698548c185d0ab31
Piper Inns Hall <110881957+piperinnshall@users.noreply.github.com>
2025-04-23 14:03:49 +1200

    chore: Cleanup (#10)
    * feat: Add score system
    
    * fix: clean up godot

--- src/scripts/conveyor.gd
+++ src/scripts/conveyor.gd
@@ -1,35 +1,30 @@
 extends Node2D
 
-@export var belt_scene: PackedScene = preload("res://src/nodes/belt.tscn")
+@onready var belt_scene: PackedScene = preload("res://src/nodes/belt.tscn")
+@onready var belt_script: Script = preload("res://src/scripts/belt.gd")
+
 @export var spacing: float = 150.0
 
 var last_belt = null
 
 func _ready() -> void:
-	propogate()
+	propagate()
 
 func _process(_delta: float) -> void:
-	if not last_belt or last_belt.global_position.x <= last_belt.spawn_position.x - spacing:
+	if not last_belt or last_belt.global_position.x <= belt_script.SPAWN_POSITION.x - spacing:
 		spawn_belt()
 
 func spawn_belt() -> void:
 	var b = belt_scene.instantiate()
-	b.z_as_relative = false
 	b.z_index = 20
 	get_tree().get_root().call_deferred("add_child", b)
 	b.setup()
 	last_belt = b
-	
-func propogate():
-	var temp_belt = belt_scene.instantiate()
-	var start_x = temp_belt.spawn_position.x
-	var end_x = temp_belt.final_position
-	temp_belt.queue_free()
 
-	var pos_x = start_x
-	while pos_x >= end_x:
+func propagate():
+	var pos_x = belt_script.SPAWN_POSITION.x
+	while pos_x >= belt_script.FINAL_POSITION:
 		var b = belt_scene.instantiate()
-		b.z_as_relative = false
 		b.z_index = 20
 		get_tree().get_root().call_deferred("add_child", b)
 		b.setup()
---
+++ src/scripts/signals.gd
@@ -0,0 +1,3 @@
+extends Node2D
+
+signal increment_score(increment: int)
